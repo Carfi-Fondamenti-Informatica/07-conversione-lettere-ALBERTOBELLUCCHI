5f5b80d1554e201866de2fb87e892ecac9f77a68
Alberto_Bellucchi <115469272+ALBERTOBELLUCCHI@users.noreply.github.com>
2022-12-05 13:41:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "lib.h"
+bool car(char &a) {
+    if (a >= 97 && a <= 122) {// minuscolo-> maiuscolo
+        a=a-32;
 
+        return true;
+    }
+
+    else if (a >= 65 && a <= 90) {//maiuscolo -> minuscolo
+        a+=32;
+
+        return true;
+    }
+
+    else return false;//numero
+}
